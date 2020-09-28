64152a4b4f472ddb47b649773a8fbe029eab7d0a
Cesar Intriago <cesar.int@outlook.com>
2020-09-27 19:31:52 -0500

    basic input and output program

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# RubyCode
+# Ruby
 Just small programs in Ruby.
 [![Run on Repl.it](https://repl.it/badge/github/ForerunnerG34/RubyCode)](https://repl.it/github/ForerunnerG34/RubyCode)
---
+++ input_output.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Ruby basic input and output commands
+puts 'Hello Human'
+puts 'What is your name?'
+name = gets.chomp
+puts "Hi #{name}, nice to meet you!"
